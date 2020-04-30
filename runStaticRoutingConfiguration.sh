6752744a4a5c28a926475d8ad5fc5830b6a3524b
pc-gh <1960128653@qq.com>
2020-04-30 11:21:19 +0800

    fourth push

--- runStaticRoutingConfiguration.sh
+++ runStaticRoutingConfiguration.sh
@@ -2,8 +2,10 @@ sudo mkdir ../second
 sudo cp ./staticRoutingConfiguration/myshixun/test1-1.sh ../
 sudo cp ./staticRoutingConfiguration/myshixun/test1-2.sh ../
 sudo cp ./staticRoutingConfiguration/myshixun/test1-3.sh ../
+sudo cp ./staticRoutingConfiguration/myshixun/gns3.net ../
 sudo cp ./staticRoutingConfiguration/myshixun/second/PC1_startup.vpc ../second/
 sudo cp ./staticRoutingConfiguration/myshixun/second/PC2_startup.vpc ../second/
 sudo cp ./staticRoutingConfiguration/myshixun/second/R1_i1_startup-config.cfg ../second/
 sudo cp ./staticRoutingConfiguration/myshixun/second/R2_i2_startup-config.cfg ../second/
 sudo cp ./staticRoutingConfiguration/myshixun/second/R3_i3_startup-config.cfg ../second/
+sudo cp ./staticRoutingConfiguration/myshixun/second/second.gns3 ../second/
