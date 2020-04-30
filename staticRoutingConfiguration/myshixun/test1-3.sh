1243afba6cf5b6ed88fe3c363a7ba2df7b45c32b
pc-gh <1960128653@qq.com>
2020-04-30 10:39:17 +0800

    fourth push

--- staticRoutingConfiguration/myshixun/test1-3.sh
+++ staticRoutingConfiguration/myshixun/test1-3.sh
@@ -1,12 +1,14 @@
 for((k=1;k<=3;k++)) 
 do   
     if [ -f "second/R${k}_i${k}_startup-config.cfg" ];then
+    
         grep -C0 "hostname\|route" second/R${k}_i${k}_startup-config.cfg
     else
         if [ -f "second/R${k}_configs_i${k}_startup-config.cfg" ];then
             grep -C0 "hostname\|route" second/R${k}_configs_i${k}_startup-config.cfg
         else
-            echo "请检查你是否将R$k的配置文件导入second目录下"
+		#grep -C0 "hostname\|route" second/R${k}_i${k}_startup-config.cfg
+           echo "请检查你是否将R$k的配置文件导入second目录下"
         fi
     fi
 done  
