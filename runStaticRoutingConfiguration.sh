c713e3125c9d95511fbf2b46cf064109094f102f
pc-gh <1960128653@qq.com>
2020-04-30 11:13:40 +0800

    fourth push

--- runStaticRoutingConfiguration.sh
+++ runStaticRoutingConfiguration.sh
@@ -1,8 +1,9 @@
-sudo mv ./staticRoutingConfiguration/myshixun/test1-1.sh ../
-sudo mv ./staticRoutingConfiguration/myshixun/test1-2.sh ../
-sudo mv ./staticRoutingConfiguration/myshixun/test1-3.sh ../
-sudo mv ./staticRoutingConfiguration/myshixun/second/PC1_startup.vpc ../
-sudo mv ./staticRoutingConfiguration/myshixun/second/PC2_startup.vpc ../
-sudo mv ./staticRoutingConfiguration/myshixun/second/R1_i1_startup-config.cfg ../second/
-sudo mv ./staticRoutingConfiguration/myshixun/second/R2_i2_startup-config.cfg ../second/
-sudo mv ./staticRoutingConfiguration/myshixun/second/R3_i3_startup-config.cfg ../second/
+sudo mkdir ../second
+sudo cp ./staticRoutingConfiguration/myshixun/test1-1.sh ../
+sudo cp ./staticRoutingConfiguration/myshixun/test1-2.sh ../
+sudo cp ./staticRoutingConfiguration/myshixun/test1-3.sh ../
+sudo cp ./staticRoutingConfiguration/myshixun/second/PC1_startup.vpc ../
+sudo cp ./staticRoutingConfiguration/myshixun/second/PC2_startup.vpc ../
+sudo cp ./staticRoutingConfiguration/myshixun/second/R1_i1_startup-config.cfg ../second/
+sudo cp ./staticRoutingConfiguration/myshixun/second/R2_i2_startup-config.cfg ../second/
+sudo cp ./staticRoutingConfiguration/myshixun/second/R3_i3_startup-config.cfg ../second/
